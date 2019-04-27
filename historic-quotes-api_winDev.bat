68ec9f4ffa93650110a8299b9a67acc17cbb15ff
hernihub <hernan_quevedo@yahoo.com>
2019-04-27 20:02:46 +1000

    comments on batch scripts

--- historic-quotes-api_winDev.bat
+++ historic-quotes-api_winDev.bat
@@ -1,3 +1,4 @@
+REM This file must be executed BEFORE the ui batch script so the folders and repository are already in place.
 mkdir C:\tmp & cd C:\tmp
 git clone https://github.com/hernihub/historic-quotes-app.git
 cd historic-quotes-app\historic-api
--- historic-quotes-ui_winDev.bat
+++ historic-quotes-ui_winDev.bat
@@ -1,2 +1,3 @@
+REM This file must be executed AFTER the api batch script was executed. Yarn should be installed.
 cd C:\tmp\historic-quotes-app\historic-ui
 yarn install & yarn run dev-server
